4b7f8b6ae948f3bafb9562dd825dc9e50dd829e6
beviggiano <beviggiano@gmail.com>
2025-03-27 18:15:42 -0300

    feat: batéria de teste de criação de usuário

--- api/src/domains/User/service/UserService.test.ts
+++ api/src/domains/User/service/UserService.test.ts
@@ -54,6 +54,47 @@ describe("UserService", () => {
 		});
 	});
 
+	describe("createUser", () => {
+		it("deve lançar QueryError se o email já estiver cadastrado", async () => {
+			prismaMock.user.findUnique.mockResolvedValueOnce({
+				...completeUserMock,
+				id: 2, // outro usuário
+				email: "teste@teste.com",
+			});
+			await expect(UserService.createUser(newUserData)).rejects.toThrow(QueryError);
+		});
+
+		it("deve lançar NotAuthorizedError se o usuário fornecer cargo", async () => {
+			await expect(UserService.createUser({ ...newUserData, role: "Member" })).rejects.toThrow(NotAuthorizedError);
+		});
+
+		it("deve lançar NotAuthorizedError se o usuário fornecer status", async () => {
+			await expect(UserService.createUser({ ...newUserData, status: "Active" })).rejects.toThrow(NotAuthorizedError);
+		});
+
+		it("deve criar um novo usuário com sucesso", async () => {
+			prismaMock.user.findUnique.mockResolvedValueOnce(null);
+			jest.spyOn(UserService, "encryptPassword").mockResolvedValueOnce("senhaCriptografada");
+			const createdUserMock = {
+				id: 1,
+				email: "teste@teste.com",
+				name: "Usuário Teste",
+				password: "senhaCriptografada",
+				photo: null,
+				key: " ",
+				cellphone: "11999999999",
+				birth: "1990-01-01",
+				status: "Pending",
+				role: null,
+				resetToken: null,
+				tokenExpires: null,
+			};
+			prismaMock.user.create.mockResolvedValueOnce(createdUserMock);
+			const result = await UserService.createUser(newUserData);
+			expect(result).toEqual(createdUserMock);
+		});
+	});
+
 
 
 });
