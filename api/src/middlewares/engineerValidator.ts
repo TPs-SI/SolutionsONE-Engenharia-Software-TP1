f03e4b0d08183e0619c3f4c9a28f3ce718784347
beviggiano <beviggiano@gmail.com>
2025-03-27 17:43:20 -0300

    feat: validação de email

--- api/src/middlewares/engineerValidator.ts
+++ api/src/middlewares/engineerValidator.ts
@@ -8,3 +8,20 @@ const dateRegex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
 const phoneRegex = /^(\+?[1-9]\d{0,2}\s?)?(\(?\d{2,3}\)?[\s-]?)?\d{4,5}[\s-]?\d{4}$/;
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
+/* ============================ */
+/*       Validações Usuário     */
+/* ============================ */
+
+// Validação de e-mail
+function emailValidation(optional = true): ValidationChain {
+  let validator = body("email");
+  if (optional) {
+    validator = validator.optional({ checkFalsy: true });
+  }
+  return validator
+    .notEmpty()
+    .withMessage("Insira seu e-mail!")
+    .isEmail()
+    .withMessage("Email inválido!");
+}
+
