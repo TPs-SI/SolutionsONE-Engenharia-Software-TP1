1b74fa56cdc57dd4cf5f2d0f6d66b3f89db131ee
beviggiano <beviggiano@gmail.com>
2025-03-27 18:14:57 -0300

    feat: config básica arquivo teste para usuários

---
+++ api/src/domains/User/service/UserService.test.ts
@@ -0,0 +1,50 @@
+// UserService.test.ts
+import bcrypt from "bcrypt";
+import { QueryError } from "../../../../errors/QueryError";
+import { NotAuthorizedError } from "../../../../errors/NotAuthorizedError";
+import { PermissionError } from "../../../../errors/PermissionError";
+import { InvalidParamError } from "../../../../errors/InvalidParamError";
+
+import UserService from "./UserService";
+import prisma from "../../../../config/prismaClient";
+import { prismaMock } from "../../../../config/singleton";
+
+// Mocks completos para o tipo User do Prisma
+const completeUserMock = {
+	id: 1,
+	email: "teste@teste.com",
+	name: "Usuário Teste",
+	password: "hashed",
+	photo: null,
+	key: " ",
+	cellphone: "11999999999",
+	birth: "1990-01-01",
+	status: "Active",
+	role: "Member",
+	resetToken: null,
+	tokenExpires: null,
+};
+
+const newUserData = {
+	id: 1,
+	email: "teste@teste.com",
+	name: "Usuário Teste",
+	password: "senhaTeste",
+	photo: null,
+	key: " ",
+	cellphone: "11999999999",
+	birth: "1990-01-01",
+	status: null,
+	role: null,
+	resetToken: null,
+	tokenExpires: null,
+};
+
+describe("UserService", () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+
+
+
+});
