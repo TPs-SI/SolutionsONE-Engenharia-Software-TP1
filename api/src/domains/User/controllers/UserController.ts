1ac6d36b89eac1f5e54cb674a815155c9b884c84
beviggiano <beviggiano@gmail.com>
2025-03-27 17:42:55 -0300

    feat: criação de middleware para validação, config básica

--- api/src/domains/User/controllers/UserController.ts
+++ api/src/domains/User/controllers/UserController.ts
@@ -4,7 +4,12 @@ import { Router, Request, Response, NextFunction} from "express";
 
 import UserService from "../service/UserService";
 import statusCodes from "../../../../utils/constants/statusCodes";
+import { validateEngineerRoute } from "../../../middlewares/engineerValidator";
 
 const UserRouter = Router();
+
+/*===== Criação de Conta ===== */
+
+
 
 export default UserRouter;
