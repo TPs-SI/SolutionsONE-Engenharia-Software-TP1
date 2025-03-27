628dc467437b04c3b26182ab0a07b4aa99bc08c4
beviggiano <beviggiano@gmail.com>
2025-03-27 17:03:22 -0300

    fix: implementado o método de ler um usuário pelo id

--- api/src/domains/User/service/UserService.ts
+++ api/src/domains/User/service/UserService.ts
@@ -1,6 +1,7 @@
 import { User } from "@prisma/client";
 import prisma from "../../../../config/prismaClient";
 
+import {selectItems, selectItems2} from "./excludeAttributes";
 import bcrypt from "bcrypt"
 import crypto from 'crypto';
 
@@ -40,26 +41,40 @@ class UserService {
         }
 
         if (body.password!=null){
-		const encrypted = await this.encryptPassword(body.password);
+			const encrypted = await this.encryptPassword(body.password);
 
-        const user = await prisma.user.create({
-            data: {
-                id: body.id,
-                email: body.email,
-                name: body.name,
-                password: encrypted,
-                key:" ",
-                cellphone: body.cellphone,
-                birth: body.birth,    
-                status: "Pending",
-                role: null,
-                resetToken: null,
-                tokenExpires: null,
-            },
+			const user = await prisma.user.create({
+				data: {
+					id: body.id,
+					email: body.email,
+					name: body.name,
+					password: encrypted,
+					key:" ",
+					cellphone: body.cellphone,
+					birth: body.birth,    
+					status: "Pending",
+					role: null,
+					resetToken: null,
+					tokenExpires: null,
+				},
+				select: selectItems
+			})
+
+			return user;
+		}
+	}
+
+	async readUser(id: number) {
+        const user = await prisma.user.findUnique({
+            where: {id: id},
             select: selectItems
-        })
+        });
+    
+        if(!user){
+            throw new QueryError("Id de usuário inexistente e/ou inválido");
+        }
 
-        return user;}}
+        return user;
     }
 
 }
