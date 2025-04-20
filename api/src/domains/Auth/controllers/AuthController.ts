83fbe8e2ceeefc7b0e9782179c90b79f81d31561
Allan Dayrell <allan.dayrell@gmail.com>
2025-04-20 18:50:32 -0300

    feat: create auth domain structure

---
+++ api/src/domains/Auth/controllers/AuthController.ts
@@ -0,0 +1,11 @@
+import { Router, Request, Response, NextFunction } from "express";
+import AuthService from "../services/AuthService";
+import statusCodes from "../../../../utils/constants/statusCodes";
+
+const authRouter = Router();
+
+class AuthController {
+
+}
+
+export default authRouter;
---
+++ api/src/domains/Auth/services/AuthService.ts
@@ -0,0 +1,9 @@
+import { User } from "@prisma/client";
+import prisma from "../../../../config/prismaClient";
+import bcrypt from "bcryptjs";
+
+class AuthService {
+
+}
+
+export default new AuthService();
