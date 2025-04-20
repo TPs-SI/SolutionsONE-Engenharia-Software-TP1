050e709651a500e688f3778f15401d7df9767320
Allan Dayrell <allan.dayrell@gmail.com>
2025-04-20 18:55:17 -0300

    feat: implement login controller and setup route

--- api/src/domains/Auth/controllers/AuthController.ts
+++ api/src/domains/Auth/controllers/AuthController.ts
@@ -5,7 +5,26 @@ import statusCodes from "../../../../utils/constants/statusCodes";
 const authRouter = Router();
 
 class AuthController {
+    // Handler para a rota de login
+    async handleLogin(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { email, password } = req.body;
 
+            if (!email || !password) {
+                return res.status(statusCodes.BAD_REQUEST).json({ error: "Email e senha são obrigatórios." });
+            }
+            const validatedUser = await AuthService.validateUserCredentials(email, password);
+
+            res.status(statusCodes.SUCCESS).json(validatedUser);
+
+        } catch (error) {
+            next(error);
+        }
+    }
 }
+
+const controller = new AuthController();
+
+authRouter.post("/login", controller.handleLogin);
 
 export default authRouter;
