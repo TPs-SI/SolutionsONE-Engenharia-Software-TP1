1657041a68d9bef29308ee7e9157a2d540ffd38b
Allan Dayrell <allan.dayrell@gmail.com>
2025-04-22 21:24:34 -0300

    feat(client): apply ProtectedRoute to application routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,43 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext"; 
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./ui/routes/ProtectedRoute";
+import LoginScreen from "./ui/screens/Login"; 
 
-import pages from "./ui/routes/pages";
+// Importar os componentes das páginas protegidas
+import ProjectsList from "./ui/screens/ProjectsList";
+import CreateProject from "./ui/screens/CreateProject";
+import SpecificProject from "./ui/screens/SpecificProject";
+import UpdateProject from "./ui/screens/UpdateProject";
+// Importar outros componentes protegidos (UserList, ContractsList, Profile/MinhaConta, etc.)
 
 function App() {
   return (
-    <AuthProvider> 
-      <div> {/* Manter a div externa se necessário */}
+    <AuthProvider>
+      <div>
         <BrowserRouter>
           <Routes>
-              {
-                pages.map((page) => (
-                  <Route key={page.link} path={page.link} element={<page.component />} />
-                ))
-              }
-            </Routes>
+            {/* Rota Pública: Login */}
+            <Route path="/login" element={<LoginScreen />} />
+            {/* Adicionar aqui outras rotas públicas como /forgot-password, /new-password/:token */}
+            
+            {/* Rotas Protegidas */}
+            <Route element={<ProtectedRoute />}> 
+              <Route path="/" element={<ProjectsList />} /> 
+              <Route path="/projects" element={<ProjectsList />} />
+              <Route path="/projects/:id" element={<SpecificProject />} />
+              <Route path="/create-project" element={<CreateProject />} />
+              <Route path="/update-project/:id" element={<UpdateProject />} />
+              {/* Adicione outras rotas protegidas aqui (Users, Contracts, Profile, etc.) */}
+              {/* <Route path="/users" element={<UserListScreen />} /> */}
+              {/* <Route path="/contracts" element={<ContractListScreen />} /> */}
+              {/* <Route path="/account" element={<ProfileScreen />} /> */}
+            </Route>
+
+          </Routes>
         </BrowserRouter>
       </div>
     </AuthProvider>
   );
 }
 
-export default App
+export default App;
