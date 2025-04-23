9b32baeffbaa6aec6ca96101ab9d143aba4f4720
Allan Dayrell <allan.dayrell@gmail.com>
2025-04-22 22:59:33 -0300

    fix: circular dependency problem

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./ui/routes/ProtectedRoute";
-import LoginScreen from "./ui/screens/Login"; 
-
-// Importar os componentes das páginas protegidas
-import ProjectsList from "./ui/screens/ProjectsList";
-import CreateProject from "./ui/screens/CreateProject";
-import SpecificProject from "./ui/screens/SpecificProject";
-import UpdateProject from "./ui/screens/UpdateProject";
-// Importar outros componentes protegidos (UserList, ContractsList, Profile/MinhaConta, etc.)
+import pageConfigs from "./ui/routes/pages"; 
+import { screenComponentMap } from "./ui/routes/route"; 
+import LoginScreen from "./ui/screens/Login";
 
 function App() {
   return (
@@ -16,22 +11,37 @@ function App() {
       <div>
         <BrowserRouter>
           <Routes>
-            {/* Rota Pública: Login */}
+            {/* Rota Pública: Login (pode ser definida manualmente ou via config) */}
             <Route path="/login" element={<LoginScreen />} />
-            {/* Adicionar aqui outras rotas públicas como /forgot-password, /new-password/:token */}
             
             {/* Rotas Protegidas */}
-            <Route element={<ProtectedRoute />}> 
-              <Route path="/" element={<ProjectsList />} /> 
-              <Route path="/projects" element={<ProjectsList />} />
-              <Route path="/projects/:id" element={<SpecificProject />} />
-              <Route path="/create-project" element={<CreateProject />} />
-              <Route path="/update-project/:id" element={<UpdateProject />} />
-              {/* Adicione outras rotas protegidas aqui (Users, Contracts, Profile, etc.) */}
-              {/* <Route path="/users" element={<UserListScreen />} /> */}
-              {/* <Route path="/contracts" element={<ContractListScreen />} /> */}
-              {/* <Route path="/account" element={<ProfileScreen />} /> */}
+            <Route element={<ProtectedRoute />}>
+              {
+                pageConfigs
+                  .filter(page => page.link !== '/login') 
+                  .map((page) => {
+                    const ComponentToRender = screenComponentMap[page.componentKey]; 
+                    if (!ComponentToRender) {
+                         console.error(`Componente não encontrado para a chave: ${page.componentKey}`);
+                         return null; 
+                    }
+                    return (
+                      <Route 
+                        key={page.link} 
+                        path={page.link} 
+                        element={<ComponentToRender />} 
+                      />
+                    );
+                  })
+              }
+                {/* Rota raiz pode precisar ser definida manualmente se não estiver em pageConfigs */}
+                 <Route path="/" element={<ProjectsList />} /> {/* Exemplo */}
+
             </Route>
+            
+            {/* Adicionar rotas públicas de recuperação de senha aqui */}
+            {/* <Route path="/forgot-password" element={screenComponentMap['ForgotPasswordScreen']} /> */}
+            {/* <Route path="/new-password/:token" element={screenComponentMap['NewPasswordScreen']} /> */}
 
           </Routes>
         </BrowserRouter>
@@ -41,3 +51,5 @@ function App() {
 }
 
 export default App;
+
+import ProjectsList from "./ui/screens/ProjectsList"; 
