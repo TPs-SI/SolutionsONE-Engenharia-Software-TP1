dea74114082697703384cfdbd0e1c1d8eb9dead8
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2025-04-28 19:20:34 -0300

    Creating seed for database

---
+++ api/prisma/seed.ts
@@ -0,0 +1,137 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient();
+
+const DEFAULT_PASSWORD = "$2b$10$3eKKl/Z6.rwE1TF.HA4xy.3OyPc0qdjNTupORRDQwp1tX2S25ZoiK"; // 1234567Ab@
+
+async function main() {
+
+    // Create users
+    await prisma.user.createMany({
+        data: [
+            {
+                name: 'Allan Dayrell',
+                email: "allan@mail.com",
+                key: "",
+                photo: "https://avatars.githubusercontent.com/u/82408363?v=4",
+                password: DEFAULT_PASSWORD
+            },
+            {
+                name: 'Bernardo Viggiano',
+                email: "bernardo@mail.com",
+                key: "",
+                photo: "https://avatars.githubusercontent.com/u/126487673?v=4",
+                password: DEFAULT_PASSWORD
+            },
+            {
+                name: 'Bruno Silva',
+                email: "bruno@mail.com",
+                key: "",
+                photo: "https://avatars.githubusercontent.com/u/134235945?v=4",
+                password: DEFAULT_PASSWORD
+            },
+            {
+                name: 'Eduardo Correia',
+                email: "eduardo@mail.com",
+                key: "",
+                photo: "https://avatars.githubusercontent.com/u/60967470?v=4",
+                password: DEFAULT_PASSWORD
+            },
+        ]
+    });
+
+    // Create contracts
+    await prisma.contract.createMany({
+        data: [
+            {
+                title: 'Contract 1',
+                nameClient: 'Client 1',
+                date: "2024-01-26",
+                value: 1000,
+                archivePath: "",
+            },
+            {
+                title: 'Contract 2',
+                nameClient: 'Client 3',
+                date: "2023-07-12",
+                value: 5000,
+                archivePath: "",
+            },
+            {
+                title: 'Contract 3',
+                nameClient: 'Client 3',
+                date: "2025-02-19",
+                value: 3000,
+                archivePath: "",
+            },
+            {
+                title: 'Contract 4',
+                nameClient: 'Client 4',
+                date: "2013-08-22",
+                value: 1000000,
+                archivePath: "",
+            },
+        ]
+    });
+
+    // Create projects
+    await prisma.project.createMany({
+        data: [
+            {
+                name: 'Project A',
+                contractId: 1,
+                date: "2024-01-26",
+            },
+            {
+                name: 'Project B',
+                contractId: 2,
+                date: "2025-02-19",
+            },
+            {
+                name: 'Project C',
+                contractId: 3,
+                date: "2021-11-16",
+            },
+            {
+                name: 'Project D',
+                contractId: 4,
+                date: "2019-07-02",
+            },
+        ]
+    });
+
+    // Create users and projects relations
+    await prisma.usersProjects.createMany({
+        data: [
+            {
+                userId: 1,
+                projectId: 1,
+                function: "Tech Lead",
+            },
+            {
+                userId: 2,
+                projectId: 1,
+                function: "Developer",
+            },
+            {
+                userId: 3,
+                projectId: 4,
+                function: "Developer",
+            },
+            {
+                userId: 4,
+                projectId: 1,
+                function: "QA",
+            },
+        ]
+    });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
