7673a899201ceaf7c110592684d680214e1f686f
Allan Dayrell <allan.dayrell@gmail.com>
2025-04-29 16:01:56 -0300

    feat(client): add routes for password recovery screens

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./ui/routes/ProtectedRoute";
-import pageConfigs from "./ui/routes/pages"; 
-import { screenComponentMap } from "./ui/routes/route"; 
+
+// Importar componentes diretamente
 import LoginScreen from "./ui/screens/Login";
+import ForgotPasswordScreen from "./ui/screens/ForgotPassword"; 
+import NewPasswordScreen from "./ui/screens/NewPassword";  
+import ProjectsList from "./ui/screens/ProjectsList";
+import CreateProject from "./ui/screens/CreateProject";
+import SpecificProject from "./ui/screens/SpecificProject";
+import UpdateProject from "./ui/screens/UpdateProject";
 
 function App() {
   return (
@@ -11,38 +17,21 @@ function App() {
       <div>
         <BrowserRouter>
           <Routes>
-            {/* Rota Pública: Login (pode ser definida manualmente ou via config) */}
+            {/* --- Rotas Públicas --- */}
             <Route path="/login" element={<LoginScreen />} />
+            <Route path="/forgot-password" element={<ForgotPasswordScreen />} />
+            <Route path="/new-password/:token" element={<NewPasswordScreen />} />
             
-            {/* Rotas Protegidas */}
+            {/* --- Rotas Protegidas --- */}
             <Route element={<ProtectedRoute />}>
-              {
-                pageConfigs
-                  .filter(page => page.link !== '/login') 
-                  .map((page) => {
-                    const ComponentToRender = screenComponentMap[page.componentKey]; 
-                    if (!ComponentToRender) {
-                         console.error(`Componente não encontrado para a chave: ${page.componentKey}`);
-                         return null; 
-                    }
-                    return (
-                      <Route 
-                        key={page.link} 
-                        path={page.link} 
-                        element={<ComponentToRender />} 
-                      />
-                    );
-                  })
-              }
-                {/* Rota raiz pode precisar ser definida manualmente se não estiver em pageConfigs */}
-                 <Route path="/" element={<ProjectsList />} /> {/* Exemplo */}
-
+              {/* Mapear rotas protegidas diretamente */}
+              <Route path="/" element={<ProjectsList />} /> 
+              <Route path="/projects" element={<ProjectsList />} />
+              <Route path="/projects/:id" element={<SpecificProject />} />
+              <Route path="/create-project" element={<CreateProject />} />
+              <Route path="/update-project/:id" element={<UpdateProject />} />
             </Route>
             
-            {/* Adicionar rotas públicas de recuperação de senha aqui */}
-            {/* <Route path="/forgot-password" element={screenComponentMap['ForgotPasswordScreen']} /> */}
-            {/* <Route path="/new-password/:token" element={screenComponentMap['NewPasswordScreen']} /> */}
-
           </Routes>
         </BrowserRouter>
       </div>
@@ -51,5 +40,3 @@ function App() {
 }
 
 export default App;
-
-import ProjectsList from "./ui/screens/ProjectsList"; 
